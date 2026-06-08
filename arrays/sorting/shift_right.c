3e974ffad121eb429e865af7c46905704dc355d2
Siva <sdadired@gitam.in>
2026-06-08 03:19:54 +0000

    shifting an array to right

---
+++ arrays/sorting/shift_right.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void shift_right(int arr[],int size){
+	int i,temp=0;
+			for(i=0;i<size/2;i++){
+			temp=arr[i];
+			arr[i]=arr[size-1-i];
+			arr[size-1-i]=temp;
+			}
+			printf("array after shifting:");
+			for(i=0;i<size;i++){
+			printf("%d",arr[i]);
+			}
+			}
+
+int main(){
+	int size;
+	printf("enter size:");
+	scanf("%d",&size);
+	
+	int arr[size],i;
+	printf("enter array elements:");
+	for(i=0;i<size;i++){
+		scanf("%d",&arr[i]);
+	}
+
+	shift_right(arr,size);
+}
